3a3c3d66e5fb3679f73d239bd2160be1df3b02d0
Deborah Lacerda <lace0115@algonquinlive.com>
2023-05-25 11:15:13 -0400

    Part 1 done

--- Pages/Registration.cshtml.cs
+++ Pages/Registration.cshtml.cs
@@ -1,36 +1,82 @@
 using AcademicManagement;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Lab_2.Pages
 {
     public class RegistrationModel : PageModel
     {
-        private readonly ILogger<RegistrationModel> _logger;
+        private const string NoStudentSelectedError = "You must select a Student!";
+        private const string StudentSelectedHasnotCourse = "The Student has not registered for any course. Select courses to register";
+        private const string StudentSelectedRegistered = "The Student has been registered for the following courses";
+        private const string NoCoursSelectedError = "You must select at least one Course!";
 
-        public RegistrationModel(ILogger<RegistrationModel> logger)
+       [BindProperty]
+        public string SelectedStudentId { get; set; } = string.Empty;
+
+        public List<AcademicRecord> RegisteredCourses { get; set; } = new List<AcademicRecord>();
+
+        public string ErrorMessage
         {
-            _logger = logger;
-
-        }
-        [BindProperty]
-        public string SelectedStudent { get; set; }
-        [BindProperty]
-        public Course SelectedCourse { get; set; }
-        public List<Course> SelectedCourses { get; set; }
-
-        [BindProperty]
-        public bool Selected { get; set; }
-
-        public void OnGet()
-        {
-        }
-        public void OnPost()
-        {
+            set
+            {
+                TempData["ErrorMessage"] = value;
+            }
         }
 
+        public string InfoMessage
+        {
+            set
+            {
+                TempData["InfoMessage"] = value;
+            }
+        }
 
+        
+        public IActionResult OnPostStudentSelected()
+        {
+            if (string.IsNullOrEmpty(SelectedStudentId))
+            {
+                ErrorMessage = NoStudentSelectedError;
+                return Page();
+            }
+
+            LoadSelectedStudentCourses();
+
+            if (RegisteredCourses.Count == 0)
+            {
+                InfoMessage = StudentSelectedHasnotCourse;
+            }
+            else
+            {
+                InfoMessage = StudentSelectedRegistered;
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPostRegister(List<string> selectedCourses)
+        {
+            if (selectedCourses == null || selectedCourses.Count == 0)
+            {
+                ErrorMessage = NoCoursSelectedError;
+                return Page();
+            }
+
+            foreach (var courseCode in selectedCourses)
+            {
+                var academicRecord = new AcademicRecord(SelectedStudentId, courseCode);
+                DataAccess.AddAcademicRecord(academicRecord);
+            }
+
+            LoadSelectedStudentCourses();
+
+            return Page();
+        }
+
+        private void LoadSelectedStudentCourses()
+        {
+            RegisteredCourses = DataAccess.GetAcademicRecordsByStudentId(SelectedStudentId);
+        }
     }
-
 }
